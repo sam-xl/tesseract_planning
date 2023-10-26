11db2df7232ce9aa1ae3e488e65c39d7be8c192c
Thomas Hettasch <thomas.hettasch@gmail.com>
2023-10-26 11:33:04 +0000

    Fix loss of first waypoint in upsample trajectory

--- tesseract_task_composer/planning/src/nodes/upsample_trajectory_task.cpp
+++ tesseract_task_composer/planning/src/nodes/upsample_trajectory_task.cpp
@@ -137,6 +137,7 @@ void UpsampleTrajectoryTask::upsample(CompositeInstruction& composite,
       if (start_instruction.isNull())
       {
         start_instruction = i.as<MoveInstructionPoly>();
+        composite.push_back(i);
         continue;
       }
 
